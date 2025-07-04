efc0941fb0df3da99685c60fd07da6e13da69f6d
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-05 04:36:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 function convertToRoman(num) {
   	const obj = {
-      0:['M',1000],
-	  1:['CM',900],
-      2:['D', 500], 
-      3:['CD', 400], 
-	  4:['C',100],	
-      5:['XC', 90], 
-      6:['L', 50], 
-      7:['XL', 40], 
-      8:['X', 10],
-	  9:['IX',9],
-	  10:['V',5],
-	  11:['IV',4],
-	  12:['I',1]
+      'M':1000,
+	  'CM':900,
+      'D':500, 
+      'CD':400,
+	  'C':100,	
+      'XC':90,
+      'L':50, 
+      'XL':40, 
+      'X':10,
+	  'IX':9,
+	  'V':5,
+	  'IV':4,
+	  'I':1
     };
-
   //your code here
 	let res="";
-	for(let[roman,value] of obj ){
-		while(num>=value[1]){
-			res+=value[0];
-			num-=value[1];
+	for(let [roman,value] of obj ){
+		while(num>=value){
+			res+=num;
+			num-=value;
 		}
 	}
 return res;
